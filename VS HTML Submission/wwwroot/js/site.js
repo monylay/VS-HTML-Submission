b8c43c198f633a12200c7e3cee26f7c609f62aa5
monylay <monylay48@gmail.com>
2026-08-15 23:53:19 -0400

    Add project files.

---
+++ VS HTML Submission/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+//animation for title
+
+let position = 0;
+let direction = 1;
+
+function animateTitle() {
+    position += direction;
+
+    if (position >= 100 || position <= 0) {
+        direction *= -1;
+    } 
+    document.getElementById("title").style.left = position + "px";
+}
+
+setInterval(animateTitle, 10);
